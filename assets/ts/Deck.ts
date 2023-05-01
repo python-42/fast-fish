75acd7488106a299dd04c51138a391968b194519
python-42 <herrmann.jake@protonmail.com>
2023-05-01 12:31:53 -0500

    Moved method implementation to CardCollection.

--- assets/ts/Deck.ts
+++ assets/ts/Deck.ts
@@ -11,50 +11,12 @@ export default class Deck extends CardCollection {
         this.shuffle();
     }
 
-    public override addCard(card : Card) : void {
-        if(this.inCardCollection(card) || this.maxCardCount === this.getCardCount()) {
-            return;
-        }
-        this.cards.push(card);
-    }
-
-    public override mergeCollections(cards : CardCollection) : void {
-        
-    }
-
-    public override addCards(newCards : Card[]) : void {
-        for (let i = 0; i < newCards.length; i++) {
-            if(! this.inCardCollection(newCards[i])) {
-                this.cards.push(newCards[i]);
-            }
-        }
-    }
-
-    public override removeCard(card : Card) : Card {
-        for (let i = 0; i < this.cards.length; i++) {
-            if(this.cards[i] === card) {
-                this.cards.splice(i, 1);
-                return card;
-            }
-        }
-        return null;
-    }
-
-    public override passCard(card : Card, target : CardCollection) : void {
-        if(!this.inCardCollection(card)) {
-            return;
-        }
-        target.addCard(card);
-        this.removeCard(card);
-
-    }
-
     private getFullDeck() : Card[] {
         let rtn : Card[] = [];
         let suits : Suit[] = [Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades];
         for (let i = 0; i < 4; i++) {
             for (let ii = 1; ii < 14; ii++) {
-                rtn.push(new Card(suits[i], Rank[ii]));
+                rtn.push(new Card(suits[i], ii));
             }
         }
         return rtn;
