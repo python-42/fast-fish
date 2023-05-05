3074165c943794c0d21e4333ea581266d5186622
python-42 <herrmann.jake@protonmail.com>
2023-05-05 08:55:55 -0500

    Fixed off by one error in getFullDeck

--- assets/ts/Deck.ts
+++ assets/ts/Deck.ts
@@ -15,7 +15,7 @@ export default class Deck extends CardCollection {
         let rtn : Card[] = [];
         let suits : Suit[] = [Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades];
         for (let i = 0; i < 4; i++) {
-            for (let ii = 1; ii < 14; ii++) {
+            for (let ii = 0; ii < 13; ii++) {
                 rtn.push(new Card(suits[i], ii));
             }
         }
