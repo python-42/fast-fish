4b0546e357ab6a86271d6eebaa34985724528c26
Michael J. Shalewski <mshalewski@outlook.com>
2023-05-10 20:23:52 +0000

    segemented the CenterBoard

--- fast-fish/src/CenterBoard.js
+++ fast-fish/src/CenterBoard.js
@@ -1,26 +1,50 @@
 import "./App.css";
 
+const PlayerName = () => {
+  return (
+    <input
+      id="PlayerName"
+      className="PlayerText"
+      type="text"
+      placeholder="Enter Player Name: "
+    />
+  );
+};
+
+const BotName = () => {
+  return (
+    <input
+      id="BotName"
+      className="PlayerText"
+      type="text"
+      placeholder="Enter Bot Name: "
+    />
+  );
+};
+
+const TurnIndicator = () => {
+  return (
+    <div className="turn">
+      <img id="turnIcon" src="./images/bot_turn.png" alt="bot icon" />
+    </div>
+  );
+};
+
+const Deck = () => {
+  return (
+    <button type="button" id="DrawCard">
+      <img id="deckIcon" src="./images/back.png" alt="Deck Icon" />
+    </button>
+  );
+};
+
 const CenterBoard = () => {
   return (
     <div className="CenterBoard">
-      <button type="button" id="DrawCard">
-        <img id="deckIcon" src="./images/back.png" alt="Deck Icon" />
-      </button>
-      <input
-        id="PlayerName"
-        className="PlayerText"
-        type="text"
-        placeholder="Enter Player Name: "
-      />
-      <div className="turn">
-        <img id="turnIcon" src="./images/bot_turn.png" alt="bot icon" />
-      </div>
-      <input
-        id="BotName"
-        className="PlayerText"
-        type="text"
-        placeholder="Enter Bot Name: "
-      />
+      <Deck />
+      <PlayerName />
+      <TurnIndicator />
+      <BotName />
     </div>
   );
 };
